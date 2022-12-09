db6f223ef0b9f599d8de74539f46bc7785b812d1
yannformation <cotardyann@orange.fr>
2022-12-09 15:37:12 +0100

    add navbar

--- HomeTech/src/app/app.module.ts
+++ HomeTech/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccueilComponent } from './components/accueil/accueil.component';
-import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { ProfilComponent } from './components/profil/profil.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent,
-    LoginComponent,
+    ProfilComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent,
 
   ],
   imports: [
--- HomeTech/src/app/components/cart/cart.component.spec.ts
+++ HomeTech/src/app/components/cart/cart.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AccueilComponent } from './accueil.component';
+import { CartComponent } from './cart.component';
 
-describe('AccueilComponent', () => {
-  let component: AccueilComponent;
-  let fixture: ComponentFixture<AccueilComponent>;
+describe('CartComponent', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AccueilComponent ]
+      declarations: [ CartComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AccueilComponent);
+    fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
