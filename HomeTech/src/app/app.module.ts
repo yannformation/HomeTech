29471922bf8e25cdf5e3477ab5f1c7ce60906c7d
yannformation <cotardyann@orange.fr>
2022-12-20 15:32:09 +0100

    css + navbar et footer z-index

--- HomeTech/src/app/app.module.ts
+++ HomeTech/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-import { ProfilComponent } from './components/profil/profil.component';
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductsComponent } from './components/products/products.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfilComponent,
     HeaderComponent,
     CartComponent,
     ProductsComponent,
@@ -28,6 +27,7 @@ import { FooterComponent } from './components/footer/footer.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    HttpClientModule
 
 
   ],
